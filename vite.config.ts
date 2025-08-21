4dc5aec13f1c7493b3bb5ae6dbea5473da46db36
Kartikay <kartikg052@gmail.com>
2025-08-21 18:05:57 +0530

    # deploy in vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Kraftbase/",
+  // base: "/Kraftbase/",
+  // "homepage": "https://Kartikay123.github.io/Kraftbase",
 });
